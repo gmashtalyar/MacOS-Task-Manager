ff96053b6b944fdcf1f17ba03dabbb2f009bae39
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-20 20:58:49 +0300

    Initial Commit

---
+++ MacOSTaskManager/MacOSTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSTaskManagerApp.swift
+//  MacOSTaskManager
+//
+//  Created by Геннадий Машталяр on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MacOSTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
